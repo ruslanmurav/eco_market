594e7fdd796be6bf9477dfed034286bd56a4d9ab
ruslanmurav <ruslanmuraveiko@gmail.com>
2024-02-09 22:11:10 +0300

    create short-product serializers

--- goods/serializers.py
+++ goods/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
-from goods.models import Category
+from goods.models import Category, Product
 
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ['id', 'name', 'photo']
+
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'name', 'photo', 'price']
