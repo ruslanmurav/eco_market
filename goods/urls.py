87fb35430ff75dd697eb1674ab65bf516c33eea4
ruslanmurav <ruslanmuraveiko@gmail.com>
2024-02-09 22:25:56 +0300

    fix bug in urls (change view name)

--- goods/urls.py
+++ goods/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from goods.views import ListCategoryAPIView, ProductListAPIView
+from goods.views import ListCategoryAPIView, ProductListAPIView, ProductDetailAPIView
 
 urlpatterns = [
     path('categories/', ListCategoryAPIView.as_view(), name='categories-list'),
     path('products/', ProductListAPIView.as_view(), name='products-list'),
-    # path('products/<int:id>', template, name='product'),
+    path('products/<int:id>', ProductDetailAPIView.as_view(), name='product'),
 ]
