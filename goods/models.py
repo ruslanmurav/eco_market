eec729394da7ee562a24e156ad795100cc8b97ef
ruslanmurav <ruslanmuraveiko@gmail.com>
2024-02-09 21:51:57 +0300

    create product models

--- goods/models.py
+++ goods/models.py
@@ -3,6 +3,11 @@ from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=30)
-    photo = models.ImageField(upload_to='uploads/photos/')
+    photo = models.ImageField(upload_to='uploads/photos/category')
 
 
+class Product(models.Model):
+    name = models.CharField(max_length=30)
+    photo = models.ImageField(upload_to='uploads/photos/products')
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    description = models.TextField()
