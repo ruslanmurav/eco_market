d1ce00233b9714daafafda51b792ac42928c9c4a
ruslanmurav <ruslanmuraveiko@gmail.com>
2024-02-09 22:24:17 +0300

    register model to admin panel

--- goods/urls.py
+++ goods/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from goods.views import ListCategoryAPIView, ProductListView
+from goods.views import ListCategoryAPIView, ProductListAPIView
 
 urlpatterns = [
     path('categories/', ListCategoryAPIView.as_view(), name='categories-list'),
-    path('products/', ProductListView.as_view(), name='products-list'),
+    path('products/', ProductListAPIView.as_view(), name='products-list'),
     # path('products/<int:id>', template, name='product'),
 ]
