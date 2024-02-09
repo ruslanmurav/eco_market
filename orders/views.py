c909ff4bb002438b3b18ceaa7817145c0ac1dda6
ruslanmurav <ruslanmuraveiko@gmail.com>
2024-02-09 23:04:46 +0300

    create OrderProduct

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,20 @@
-from rest_framework.generics import CreateAPIView
+from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView
 
 from orders.models import Order
-from orders.serializers import OrderCreateSerializer
+from orders.serializers import OrderCreateSerializer, OrderShortSerializer, OrderDetailSerializer
 
 
 class OrderCreateAPIView(CreateAPIView):
     queryset = Order.objects.all()
     serializer_class = OrderCreateSerializer
+
+
+class OrderListAPIView(ListAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderShortSerializer
+
+
+class OrderDetailAPIView(RetrieveAPIView):
+    queryset = Order.objects.all()
+    serializer_class = OrderDetailSerializer
+    lookup_field = 'id'
