aa86940a971c23a20499408f71cd949e88488e4e
ruslanmurav <ruslanmuraveiko@gmail.com>
2024-02-09 22:01:16 +0300

    connect to mysql server

--- eco_market/settings.py
+++ eco_market/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'eco_market.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'goods',
+        'USER': 'root',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- goods/urls.py
+++ goods/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
+from goods.views import template
+
 urlpatterns = [
-    path('categories/', ..., name='categories-list'),
-    path('products/', ..., name='products-list'),
-    path('products/<int:id>', ..., name='product'),
+    path('categories/', template, name='categories-list'),
+    path('products/', template, name='products-list'),
+    path('products/<int:id>', template, name='product'),
 ]
