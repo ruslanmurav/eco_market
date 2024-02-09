8cea9e64b110834e529f44a5d9ee117473a37f29
ruslanmurav <ruslanmuraveiko@gmail.com>
2024-02-09 22:22:11 +0300

    create product view

--- goods/urls.py
+++ goods/urls.py
@@ -1,10 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from goods.views import ListCategoryAPIView
+from goods.views import ListCategoryAPIView, ProductListView
 
 urlpatterns = [
     path('categories/', ListCategoryAPIView.as_view(), name='categories-list'),
-    path('products/', template, name='products-list'),
-    path('products/<int:id>', template, name='product'),
+    path('products/', ProductListView.as_view(), name='products-list'),
+    # path('products/<int:id>', template, name='product'),
 ]
--- goods/views.py
+++ goods/views.py
@@ -1,11 +1,18 @@
 from rest_framework.generics import ListAPIView
 
-from goods.models import Category
-from goods.serializers import CategorySerializer
+from goods.models import Category, Product
+from goods.serializers import CategorySerializer, ShortProductSerializer
 
 
 class ListCategoryAPIView(ListAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+class ProductListView(ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ShortProductSerializer
+
+
 
 
