82b94d9c669930d36a3ce129b506e1f9f135290b
ruslanmurav <ruslanmuraveiko@gmail.com>
2024-02-09 21:46:37 +0300

    create endpoints for goods

--- goods/urls.py
+++ goods/urls.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
-
+    path('categories/', ..., name='categories-list'),
+    path('products/', ..., name='products-list'),
+    path('products/<int:id>', ..., name='product'),
 ]
