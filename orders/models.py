8e99d1bb0e474db09f384c2da3122979fcd1f8d5
ruslanmurav <ruslanmuraveiko@gmail.com>
2024-02-09 22:54:11 +0300

    create order models with MtM relationship

---
+++ orders/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+
+from goods.models import Product
+
+
+# Create your models here.
+
+
+class Order(models.Model):
+    phone_number = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
+    place = models.CharField(max_length=100)
+    comment = models.TextField()
+    products = models.ManyToManyField(Product, through='OrderProduct')
+
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
---
+++ orders/views.py
@@ -0,0 +1,4 @@
+
+
+def temp(request):
+    pass
