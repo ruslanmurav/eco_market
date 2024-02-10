d1fd4efe1f11f7cbe3226bc25918531688dbc2a9
ruslanmurav <ruslanmuraveiko@gmail.com>
2024-02-10 11:17:55 +0300

    fix bug which displayed only product id

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 
-from orders.models import Order
+from goods.serializers import FullProductSerializer
+from orders.models import Order, OrderProduct
 
 
 class OrderCreateSerializer(serializers.ModelSerializer):
@@ -10,12 +11,20 @@ class OrderCreateSerializer(serializers.ModelSerializer):
 
 
 class OrderDetailSerializer(serializers.ModelSerializer):
+    products = FullProductSerializer(many=True)
+
     class Meta:
         model = Order
-        exclude = []
+        fields = ['id', 'created_at', 'products']
 
 
 class OrderShortSerializer(serializers.ModelSerializer):
+    total_cost = serializers.SerializerMethodField()
+
+    def get_total_cost(self, obj):
+        total = sum(item.product.price * item.quantity for item in obj.orderproduct_set.all())
+        return total
+
     class Meta:
         model = Order
         fields = ['id', 'created_at', 'total_cost']
